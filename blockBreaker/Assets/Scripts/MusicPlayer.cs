7a55a118363b8c9e97c3838f1056c9b3037180e7
AvidRP <sauhardpant98@gmail.com>
2017-08-07 21:23:12 -0600

    resolved music transition glitch

---
+++ blockBreaker/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+	static MusicPlayer instance = null;
+	// Use this for initialization
+	
+	void Awake () {
+		Debug.Log ("Music Player Awake " + GetInstanceID());
+	}
+	void Start () {
+		Debug.Log ("Music Player Start " + GetInstanceID());
+		
+		if(instance != null) {
+			Destroy(gameObject);
+		} else {
+			instance = this;
+			GameObject.DontDestroyOnLoad(gameObject);
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
